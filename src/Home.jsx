e82c0a04dfa99cc790b66dfddf2ed597c2ba3195
insanecoder09 <147096842+insanecoder09@users.noreply.github.com>
2024-06-10 13:20:32 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
       appLogo:
         "https://res.cloudinary.com/dhj9wvmmo/image/upload/v1716776157/Rummy-Logos/WhatsApp_Image_2024-05-26_at_5.54.36_AM_lmvele.jpg",
       downloadLink:
-        "https://rummy888999.com//?from_gameid=14144156&channelCode=2799582",
+        "https://rummy888999.com/?from_gameid=16969896&channelCode=100000",
     },
     {
       name: "Rummy Perfect",
@@ -293,7 +293,7 @@ function Home() {
       appLogo:
         "https://res.cloudinary.com/dhj9wvmmo/image/upload/v1716776157/Rummy-Logos/WhatsApp_Image_2024-05-26_at_5.54.36_AM_lmvele.jpg",
       downloadLink:
-        "https://rummy888999.com//?from_gameid=14144156&channelCode=2799582",
+        "https://rummy888999.com/?from_gameid=16969896&channelCode=100000",
     },
     {
       name: "Rummy Perfect",
